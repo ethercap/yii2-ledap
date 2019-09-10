4b89233fafc5ad3b650e124bbe6e9a9127e325df
lishipeng <lishipeng@ethercap.com>
2019-09-10 11:05:11 +0800

    添加toast和modal

--- src/assets/LedapAsset.php
+++ src/assets/LedapAsset.php
@@ -9,6 +9,7 @@ class LedapAsset extends AssetBundle
     public $sourcePath = __DIR__;
     public $js = [
         YII_DEBUG ? './static/lib/ledap.js' : './static/lib/ledap.min.js',
+        'https://unpkg.com/vue-toasted',
     ];
     public $depends = [
         '\ethercap\ledap\assets\VueAsset',
--- src/assets/static/ledap-init.js
+++ src/assets/static/ledap-init.js
@@ -34,3 +34,57 @@ ledap.App.config({
     },
 });
 
+ledap.App.getTheme().addComponent({
+    name : 'modal',
+    props: {
+        show:{
+            default: false,
+        },
+        size:{
+            type: String,
+            default : ""
+        }
+    },
+    methods:{
+        close: function(){
+            this.$emit("update:show",!this.show);
+        },
+        clickBg: function(event){
+            if(event.target.getAttribute("role") === "modal") {
+                this.close();
+            }
+        }
+    },
+    computed: {
+        modalSize: function(){
+            return this.size === "" ? "" : "modal-"+this.size;
+        }
+    },
+    template: `<div>
+        <div class="fade modal" @click="clickBg" :class="{in:show}" role="modal" tabindex="-1" :style="{display:show? 'block' : 'none'}">
+            <div class="modal-dialog" :class="[modalSize]">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" @click="close">×</button>
+                        <slot name="header"></slot>
+                    </div>
+                    <div class="modal-body">
+                        <slot></slot>
+                    </div>
+                    <slot name="footer">
+                        <div class="modal-footer">
+                        </div>
+                    </slot>
+                </div>
+            </div>
+        </div>
+        <div class="modal-backdrop fade in" v-if="show" @click="close"></div>
+    </div>`,
+});
+
+Vue.use(Toasted, {
+    duration: 5000,
+    keepOnHover: true,
+    iconPack : 'fontawesome',
+    position: 'top-center',
+});
