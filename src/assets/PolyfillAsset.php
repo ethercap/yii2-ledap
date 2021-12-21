06c4d7a9b8a9b2caf1128b31aa330d8db3d7cb6a
Fynn Tang <fzpying@gmail.com>
2021-12-21 17:38:07 +0800

    fix: 删除无效的$sourcePath (#9)
    * feat: 升级`@fortawesome/fontawesome-free`版本,`5.13.0`至`5.15.4`
    
    * feat: 1. 移除 composer 中 `npm-asset/vue`组件改为直接用 'vue.js官网推荐' CDN直接访问
    
    * feat: 移除 composer 中 `npm-asset/vue`
    
    * feat: 移除 composer 中 `npm-asset/bootstrap` 包,将本地调用改为远程CDN调用
    
    * fix: 判断不正确
    
    * feat: 移除 composer 中 `npm-asset/bootstrap-vue` 包,将本地调用改为远程CDN调用
    
    * feat: 移除 composer 中 `npm-asset/ledap` 包,将本地调用改为远程CDN调用
    
    * feat: 移除 composer 中 `npm/ledap-vue-bootstrap` 包,将本地调用改为远程CDN调用
    
    * feat: 在 `.gitignore` 中忽略`composer.lock`
    
    * fix: 将第三方cdn资源替换为本地资源
    
    * fix: 删除`$basePath`&`$baseUrl`属性，修正`$sourcePath`属性
    
    * fix: 为每一个资源设置相互独立的`$sourcePath` 以便在`web/assets`中相互独立
    
    * fix: 添加非混淆压缩源码文件
    
    * fix: 删除无效的`$sourcePath`
    
    Co-authored-by: Tmm <tangmengmeng@chuanyuapp.com>

--- src/assets/PolyfillAsset.php
+++ src/assets/PolyfillAsset.php
@@ -6,7 +6,6 @@ use yii\web\AssetBundle;
 
 class PolyfillAsset extends AssetBundle
 {
-    public $sourcePath = '@npm/bootstrap-vue/dist';
 
     public $js = [
         '//polyfill.io/v3/polyfill.js?features=es5,es6,es7&flags=gated',
